bb7426c522c7fef2d56eabee15f8d82a536990e8
O. R. Lissenberg <olissenberg@gmail.com>
2014-03-07 01:52:54 +0100

    MySQL

--- moredots/aliases.sh
+++ moredots/aliases.sh
@@ -16,6 +16,9 @@ alias mysqlroot="mysql -u root -p"
 alias stash="cd ~/Stash/"
 alias personal="cd ~/Personal/Projects"
 
+alias mysql-show-dbs="mysql -u root -p -e 'show databases;' -s -N"
+alias mysql-show-users="mysql --user=root -p -e 'select user, host from user;' --database=mysql"
+
 function am-create-library () {
     if [[ ! $# -eq 3 ]] 
     then
@@ -40,5 +43,25 @@ function am-migrate-library () {
             echo "Artisan not found.";
         fi
     fi
+}
+
+function mysql-db-user-host-pw () {
+    HASDB=$(mysql -u root -p -e "show databases like '$1';" -s -N);
+    if [[ $HASDB != $1 ]]; then
+        mysql --user=root -p -e 'CREATE DATABASE IF NOT EXISTS '$1';'
+    fi
+
+    mysql --user=root -p -e 'GRANT ALL PRIVILEGES ON '$1'.* TO '$2'@'$3' IDENTIFIED BY "'$2'";'
+}
+
+function mysql-has-db () {
+    HASDB=$(mysql -u root -p -e "show databases like '$1';" -s -N);
+    if [[ $HASDB == $1 ]]; then
+        echo $HASDB;
+    fi
+}
+
+function mysql-user-setpassword () {
+    mysql --user=root -p -e "SET PASSWORD FOR '"$1"'@'"$2"' = PASSWORD('"$3"'); FLUSH PRIVILEGES;";
 }
 
